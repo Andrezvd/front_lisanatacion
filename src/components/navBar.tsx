dd6af5cdd1f87bb42f5a9834d4e961303fb0e423
Andrezvd <andreziwis@gmail.com>
2025-04-14 16:59:45 -0500

    rework de la nav bar, ahora es mas estetica, revisar el read-me para añadir los react icons

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,39 +1,43 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/navBar.css";
-import logo from "../assets/icons/ligaicon.ico"
+import logo from "../assets/logo/Logogpt.png";
+import { FaSearch } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
+
+
 const NavBar: React.FC = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
     <div className="contenedor-navbar">
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <Link to="/">Liga de natacion Santander</Link>
-      </div>
-      <div className="navbar-logo-center">
-          <img src={logo} alt="Logo" />
-        </div>
-      <ul className="navbar-links">
-        <li><Link to="/carreras">Carreras</Link></li>
-        <li><Link to="/about">Torneos</Link></li>
-        <li><Link to="/services">Cursos</Link></li>
-        <li><Link to="/galeria">Galeria</Link></li>
-        <li 
-          className="dropdown"
-          onMouseEnter={() => setIsDropdownOpen(true)}
-          onMouseLeave={() => setIsDropdownOpen(false)}
-        >
-          <span className="dropbtn">Ingresar</span>
-          {isDropdownOpen && (
-            <ul className="dropdown-content">
-              <li><Link to="/ingreso/entrenador">Soy Entrenador</Link></li>
-              <li><Link to="/ingreso/administrador">Soy Administrador</Link></li>
-            </ul>
-          )}
-        </li>
-      </ul>
-    </nav>
+      <nav className="navbar">
+          <div className="navbar-logo">
+            <Link to="/"><img src={logo} alt="Logo" /></Link>
+          </div>
+        <ul className="navbar-links">
+          <li><Link to="/nosotros">Sobre la Liga</Link></li>
+          <li><Link to="/buscar"><FaSearch size={20} color="white" /></Link></li>
+          <li><Link to="/">Calendario</Link></li>
+          <li><Link to="/carreras">Carreras <IoIosArrowDown size={16} color="white"/></Link></li>
+          <li><Link to="/about">Torneos</Link></li>
+          <li><Link to="/services">Cursos</Link></li>
+          <li><Link to="/galeria">Galeria</Link></li>
+          <li
+            className="dropdown"
+            onMouseEnter={() => setIsDropdownOpen(true)}
+            onMouseLeave={() => setIsDropdownOpen(false)}
+          >
+            <span className="dropbtn">Iniciar sesión</span>
+            {isDropdownOpen && (
+              <ul className="dropdown-content">
+                <li><Link to="/ingreso/entrenador">Soy Entrenador</Link></li>
+                <li><Link to="/ingreso/administrador">Soy Administrador</Link></li>
+              </ul>
+            )}
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
--- src/templates/home.tsx
+++ src/templates/home.tsx
@@ -1,15 +1,11 @@
 import NavBar from "../components/navBar";
 import Footer from "../components/footer";
-import VerUsuarios from "../components/gestion_deportistas/verUsuarios";
-import CrearUsuario from "../components/gestion_deportistas/crearUsuario";
 
 function Home() {
   return (
     <>
       <NavBar/>
       <div className="espacioGrande"></div>
-      <CrearUsuario/>
-      <VerUsuarios/>
       <Footer/>
     </>
   );
